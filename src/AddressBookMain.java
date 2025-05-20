b4def26a0b83ed7d6d965234657c76e4a1c46247
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2025-05-20 10:54:52 +0530

    create contacts in address book

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -2,5 +2,10 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
         System.out.println();
+        Contact contact = new Contact(
+                "John", "Doe", "123 Elm Street", "Springfield",
+                "Illinois", 62704, "1234567890", "john.doe@example.com"
+        );
+        System.out.println(contact);
     }
 }
